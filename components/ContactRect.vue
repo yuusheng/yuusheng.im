3b9a47d0ac0243440320b311d0cedcb47c42fd7c
yuusheng <hi@yuusheng.im>
2024-04-22 14:11:41 +0800

    feat: contact rect hover reaction

--- components/ContactRect.vue
+++ components/ContactRect.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts">
 const contactList = [
-  { link: 'https://github.com/yuusheng', icon: 'i-ri-github-fill', title: 'GitHub', color: 'white' },
-  { link: 'https://twitter.com/yuusheng7', icon: 'i-ri-twitter-fill', title: '', color: 'blue' },
-  { link: 'https://mas.to/@yuusheng', icon: 'i-ri-mastodon-fill', title: '', color: 'blue' },
-  { link: 'mailto:hi@yuusheng.im', icon: 'i-ri-at-fill', title: '', color: 'blue' },
+  {
+    link: 'https://github.com/yuusheng',
+    icon: 'i-ri-github-fill',
+    title: 'GitHub',
+    classes: 'hover:bg-black dark:hover:bg-white dark:hover:text-black',
+  },
+  {
+    link: 'https://twitter.com/yuusheng7',
+    icon: 'i-ri-twitter-x-fill',
+    title: '',
+    classes: 'hover:bg-black dark:hover:bg-white dark:hover:text-black',
+  },
+  { link: 'https://mas.to/@yuusheng',
+    icon: 'i-ri-mastodon-fill',
+    title: '',
+    classes: 'hover:bg-[#5b52e1]',
+  },
+  { link: 'mailto:hi@yuusheng.im',
+    icon: 'i-ri-at-fill',
+    title: '',
+    classes: 'hover:bg-[linear-gradient(#3c7aeb,#5bbff6)] ',
+  },
 ]
 </script>
 
@@ -15,11 +33,12 @@ const contactList = [
       :href="contact.link"
       target="_blank"
       flex="~ items-center"
-      p="x1.8 y1" m="r2.4" text-xl
+      p="x1.8 y1" m="r2.4" text-xl hover:text-white
       bg="#d0d0d040" rounded
+      :class="contact.classes"
     >
       <div :class="contact.icon" />
-      <span v-if="contact?.title" ml-1>{{ contact?.title }}</span>
+      <span v-if="contact.title" ml-1>{{ contact.title }}</span>
     </a>
   </div>
 </template>
