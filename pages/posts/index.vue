29d269d1a9224fb330a51f1e8949916d684e2504
yuusheng <hi@yuusheng.im>
2025-02-27 00:39:38 +0800

    chore: lint

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -21,8 +21,10 @@ useGsap(() =>
       <div text="8xl stroke-1 stroke-warm-gray/60 transparent" font-serif absolute top-0 cursor-none z--1>
         Yuusheng's
       </div>
-      <NuxtLink v-for="post in posts" :key="post.path" class="post" :to="`/posts${post.path}`" flex="~ gap-3"
-        essay-list>
+      <NuxtLink
+        v-for="post in posts" :key="post.path" class="post" :to="`/posts${post.path}`" flex="~ gap-3"
+        essay-list
+      >
         <span text="18px">{{ post.title }}</span>
         <span text="warm-gray/70">
           {{ post.date ? $dayjs(post.date).format('MMMM D, YYYY') : '' }}
