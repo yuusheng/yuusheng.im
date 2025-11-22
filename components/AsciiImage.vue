9604bc213aeec63131ced36c5c244880f7db6393
yuusheng <yuusheng7@gmail.com>
2025-11-22 18:31:29 +0800

    feat: ascii avatar

---
+++ components/AsciiImage.vue
@@ -0,0 +1,9 @@
+<template>
+  <img src="/avatar-ascii.png" class="ascii-art-img size-full">
+</template>
+
+<style scoped>
+.ascii-art-img {
+  image-rendering: pixelated;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,7 @@
     <div class="container border-x">
       <div class="border-x-1 relative grid aspect-[24/12] grid-rows-12 border-gray-200/20 " style="grid-template-columns: repeat(24, 1fr);">
         <UiHeaderBg class="-z-1" />
-        <article class="z-10 col-[3/18] row-[1/-1] flex flex-col justify-center">
+        <article class="z-10 col-[3/16] row-[1/-1] flex flex-col justify-center">
           <h1 class="mb-3 text-6xl font-bold">
             Hi!
           </h1>
@@ -23,6 +23,9 @@
           <p>A slash youth.</p>
           <ContactRect />
         </article>
+        <div class="z-10 col-[16/-1] row-[1/-1] grid grid-cols-6 grid-rows-6">
+          <AsciiImage class="col-[2/5] row-[2/5]" />
+        </div>
       </div>
     </div>
 
